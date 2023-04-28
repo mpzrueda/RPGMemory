defcd3bc2d85057c9e5dbb92bcc3ada65d6cfbe4
RoraAu18 <91167758+RoraAu18@users.noreply.github.com>
2023-04-27 22:43:25 -0500

    Initial Spawner ready

--- RPGMemory/Assets/Scripts/CardSpawner.cs
+++ RPGMemory/Assets/Scripts/CardSpawner.cs
@@ -4,12 +4,39 @@ using UnityEngine;
 
 public class CardSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //public CardTest[] cardTypes = new CardTest[8];
+    [SerializeField]
+    SpawnSpot[] spots = new SpawnSpot[7];
+    [SerializeField]
+    Pool<CardTest> pool;
+    [SerializeField]
+    float cardFloatHeight;
+    private void Awake()
+    {
+        spots = GetComponentsInChildren<SpawnSpot>();
+        for (int i = 0; i < spots.Length; i++)
+        {
+            var parent = spots[i];
+            pool.parents.Add(parent);
+        }
+    }
     void Start()
     {
-        
+
+        pool.Init();
+
+        FillSpots();
     }
 
+    void FillSpots()
+    {
+        for (int i = 0; i < spots.Length; i++)
+        {
+            var newItem = pool.ActivatingItem();
+            newItem.transform.position = spots[i].transform.position + Vector3.up * cardFloatHeight;
+            newItem.gameObject.SetActive(true);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
--- RPGMemory/Assets/Scripts/Pool.cs
+++ RPGMemory/Assets/Scripts/Pool.cs
@@ -1,18 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using Random = UnityEngine.Random;
 
-public class Pool : MonoBehaviour
+[Serializable]
+public class Pool<CardTest>
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    GameObject[] prefabs = new GameObject[8];
+    public List<SpawnSpot> parents = new List<SpawnSpot>();
+    [SerializeField]
+    int initSize;
+    [SerializeField]
+    List<CardTest> itemsInPool = new List<CardTest>();    
+    [SerializeField]
+    List<bool> activeItems = new List<bool>();
+
+    public void Init()
     {
-        
+        GrowPool(initSize);
+    }
+    void GrowPool(int growBy)
+    {
+        for (int i = 0; i <  parents.Count; i++)
+        {
+            var randomCard = Random.Range(0, prefabs.Length);
+            var newItem = GameObject.Instantiate(prefabs[randomCard], parents[i].transform);
+            newItem.gameObject.SetActive(false);
+            itemsInPool.Add(newItem.GetComponent<CardTest>());
+            activeItems.Add(false);
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public CardTest ActivatingItem()
     {
-        
+        for (int i = 0; i < itemsInPool.Count; i++)
+        {
+            if (!activeItems[i])
+            {
+                activeItems[i] = true;
+                
+                return itemsInPool[i];
+            }
+        }
+        var lastItemitem = itemsInPool.Count;
+        GrowPool(initSize);
+        activeItems[lastItemitem] = true;
+        return itemsInPool[lastItemitem];
+    }
+
+    public void Recycling(CardTest usedItem)
+    {
+        var idx = itemsInPool.IndexOf(usedItem);
+        activeItems[idx] = false;
+
     }
 }
